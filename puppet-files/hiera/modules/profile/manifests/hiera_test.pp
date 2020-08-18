92d8529389363df7e96fa12e526e7c9741c79faa
nevin-cleetus <61216766+nevin-cleetus@users.noreply.github.com>
2020-08-18 11:28:37 +0530

    Update hiera_test.pp

--- puppet-files/hiera/modules/profile/manifests/hiera_test.pp
+++ puppet-files/hiera/modules/profile/manifests/hiera_test.pp
@@ -3,18 +3,25 @@ class profile::hiera_test (
   Boolean             $backups_enabled,
   Optional[String[1]] $site_alias = undef,
 ) {
+
+  $test_ssl    = hiera('profile::hiera_test::ssl',Boolean)
+  $backup_enabled  = lookup('profile::hiera_test::backups_enabled',Boolean)
+
+
+
   file { '/tmp/hiera_test.txt':
     ensure  => file,
     content => @("END"),
                Data from profile::hiera_test
                -----
                profile::hiera_test::ssl: ${ssl}
+               -----
+               ${test_ssl}
                profile::hiera_test::backups_enabled: ${backups_enabled}
+               ${backup_enabled}
                profile::hiera_test::site_alias: ${site_alias}
                |END
     owner   => root,
     mode    => '0644',
   }
 }
-~
-
